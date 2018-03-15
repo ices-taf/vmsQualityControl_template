6c6f816c32e3cb5687226aada341f707cbf98549
colin millar <colinpmillar@gmail.com>
2018-03-16 00:09:44 +0100

    Base whole thing on fixed file names
    So no need for the Rmd knitting for each country!

--- report.R
+++ report.R
@@ -10,29 +10,10 @@
 library(icesTAF)
 library(rmarkdown)
 
-# source utility functions
-source("utils.R")
-
-# read and parse template file
-qc <- readLines("report.Rmd")
-loc1 <- grep("<!-- QCTEMPLATE: header -->", qc)
-loc2 <- grep("<!-- QCTEMPLATE: data -->", qc)
-loc3 <- grep("<!-- QCTEMPLATE: body -->", qc)
-qc <- list(yaml = qc[1:(loc1-1)],
-           header = qc[loc1:(loc2-1)],
-           data = qc[loc2:(loc3-1)],
-           body = qc[loc3:length(qc)])
-
 # create report directory
 mkdir("report")
 
-# setup file name
-fname <- paste0("report_QC", format(Sys.time(), "_%Y-%m-%d_%b-%Y"),".Rmd")
-
-# fillin and write template
-cat(makeQCRmd(qc), sep = "\n", file = fname)
-
 # run template
-render(fname, output_dir = "report")
+render("report.Rmd", output_dir = "report")
 
 # done!
