8af251bdcf71be3bac854eddbe92a6a019881a59
Colin Millar <colin@ices.dk>
2018-05-16 15:23:35 +0200

    update to recent changes from wg_WGSFD

--- report.R
+++ report.R
@@ -1,24 +1,42 @@
-## Prepare plots/tables for report
 
-## Before:
-## After:
 
 # install vmstools from github
 #devtools::install_github("nielshintzen/vmstools/vmstools")
 
-# packages
-library(icesTAF)
+# libraries
 library(rmarkdown)
+library(icesTAF)
+library(jsonlite)
+
+# utiities
+source("QC/utilities.R")
+
+# settings
+config <- read_json("QC/config.json", simplifyVector = TRUE)
 
 # create report directory
-mkdir("report")
+mkdir(config$report_dir)
 
-# run template
-render("report.Rmd")
+# loop over countries
+for (country in config$countries) {
+  #country <- "IRL"
+  msg("Running QC for ... ", country)
+  
+  # fillin and write template
+  fname <- makeQCRmd(country, config$data_dir, template = "QC/report-QC-template.Rmd")
 
-# clean up
-cp(report.pdf, "report", move = TRUE)
-unlink("report.tex")
-unlink("report_files", recursive = TRUE)
-
-# done!
+  # render Rmd
+  ret <- try(render(fname, clean = FALSE, output_format = latex_document()))
+  if (inherits(ret, "try-error")) {
+    msg("FAILED - ", country)
+    next
+  }
+  
+  # compile pdf
+  shell(paste('pdflatex -halt-on-error', ret))
+  
+  # copy report and Rmd file
+  copyReport(fname, report_dir = config$report_dir, keeps = "pdf")
+  
+  msg("Done ... ", country)
+}
