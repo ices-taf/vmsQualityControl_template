47210a8859658aeb66fc155a0c690a5fd3d7aa9b
colinpmillar <colin.millar@ices.dk>
2020-12-08 10:50:17 +0100

    switch to icesVMS for data access

--- bootstrap/vms-data.R
+++ bootstrap/vms-data.R
@@ -11,36 +11,26 @@
 #' @tafSource script
 
 # packages
-library(RODBC)
 library(icesTAF)
+taf.library(icesVMS)
 library(jsonlite)
+
+# NOTE TO USER: check a token is present
+# icesConnect::ices_token()
 
 # settings
 config <- read_json(taf.data.path("config.json"), simplifyVector = TRUE)
 
-# connect to DB
-conn <- odbcDriverConnect(connection = config$db_connection)
-
+# download logbook from web service
 msg("downloading LE data for ... ", config$country)
-
-# set up sql command
-sqlq <- sprintf("SELECT * FROM dbo._ICES_VMS_Datacall_LE WHERE country = '%s' order by year, ICES_rectangle, gear_code, LE_MET_level6, month, vessel_length_category, fishing_days, vms_enabled", config$country)
-
-# fetch
-logbook <- sqlQuery(conn, sqlq)
+logbook <- get_logbook(country = config$country, year = 2009:(config$year-1)) # add data call year
 
 # save to file
 write.taf(logbook)
 
+# download VMS from web service
 msg("downloading VMS data for ... ", config$country)
+vms <- get_vms(country = config$country, year = 2009:(config$year-1), datacall = config$year)
 
-# set up sql command
-sqlq <- sprintf("SELECT * FROM [dbo].[_ICES_VMS_Datacall_VMS] WHERE country = '%s' order by year, c_square, gear_code, LE_MET_level6, month, vessel_length_category, fishing_hours, avg_fishing_speed", config$country)
-
-# fetch
-vms <- sqlQuery(conn, sqlq)
 # save to file
 write.taf(vms)
-
-# disconnect
-odbcClose(conn)
